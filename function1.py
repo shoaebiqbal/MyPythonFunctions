ad6bb36107ae1a06ddeaa4b1f5470f7c7146142e
shoaebiqbal <mshaoebiqbal@gmail.com>
2026-07-21 07:12:54 +0530

    function1.py chnaged with time, new file function2.py created

--- function1.py
+++ function1.py
@@ -1,5 +1,6 @@
-def Message(user_name,class_name):
+def Message(user_name,class_name,class_time):
   print(f"Hello! Mr. {user_name}")
   print(f"you are admitted in class {class_name}")
+  print(f"Your Class time is {class_time}")
 
-Message("Adnan Khan","Python Full Stack")
+Message("Adnan Khan","Python Full Stack","6:00 to 7:00")
---
+++ function2.py
@@ -0,0 +1,4 @@
+def calculate(n1,n2):
+    print(f"Addition = {n1+n2}")
+
+calculate(5,40)
